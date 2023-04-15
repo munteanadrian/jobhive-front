ad70af6e2939e3bf35d7f3f14e7270881221cc50
Adrian <amuntean2000@gmail.com>
2023-04-15 14:49:52 +0300

    added navbar

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        themeBlue: "#3498DB",
+        themeRed: "#E74C3C",
+        themeGreen: "#2ECC71",
+        themeYellow: "#F1C40F",
+
+        themeDark: "#2C3E50",
+        themeLight: "#C0C0C0",
+        themeMedium: "#818183",
+        themeBackground: "#ECF0F1",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
